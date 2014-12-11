833ad51c7703684c080e6e787e70fcb7bad331b6
Tessa <tessakkelly@gmail.com>
2014-12-10 17:53:54 -0800

    add links to album pages

--- server.rb
+++ server.rb
@@ -6,10 +6,10 @@ require_relative 'songify.rb'
 
 album_repo = Songify::Repo::AlbumRepo.new
 album_repo.create_table
-new_album = Songify::Album.new("Test Album", "TEST")
-album_repo.add(new_album)
-new_album = Songify::Album.new("Test Album2", "TEST")
-album_repo.add(new_album)
+# new_album = Songify::Album.new("Test Album", "TEST")
+# album_repo.add(new_album)
+# new_album = Songify::Album.new("Test Album2", "TEST")
+# album_repo.add(new_album)
 
 set :bind, '0.0.0.0' # This is needed for Vagrant
 
@@ -22,4 +22,9 @@ post '/' do
   new_album = Songify::Album.new(params["new-album"], params["about-new-album"])
   album_repo.add(new_album)
   redirect to('/')
+end
+
+get '/albums/:id' do
+  @albums = album_repo.get_by_id([:id])
+  erb :index
 end
